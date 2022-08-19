97d7d8bd6da0984a2eaf74bc0804c204f9d4db35
FeryazHacimuradov <tu201906125@code.edu.az>
2022-08-19 23:06:09 +0400

    added icon

--- src/components/EmployeesList/EmployeesList.jsx
+++ src/components/EmployeesList/EmployeesList.jsx
@@ -4,7 +4,7 @@ import './employeesList.css';
 
 const EmployeesList = () => {
     return (
-        <ul className="app-list list-group">
+        <ul className="app-list list-group mx-48">
             <EmployeesListItem/>
             <EmployeesListItem/>
             <EmployeesListItem/>
--- src/components/EmployeesListItem/EmployeesListItem.jsx
+++ src/components/EmployeesListItem/EmployeesListItem.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import './employeesListItem.css';
 
 const EmployeesListItem = () => {
-  return (
-    <div>EmployeesListItem</div>
-  )
+    return (
+        <li className="list-group-item flex justify-between">
+            <span className="list-group-item-label">John Smith</span>
+            <input type="text" className="list-group-item-input" defaultValue="1000$"/>
+            <div className='flex justify-center items-center'>
+                <button type="button"
+                    className="btn-cookie btn-sm ">
+                    <i className="fas fa-cookie"></i>
+                </button>
+
+                <button type="button"
+                        className="btn-trash btn-sm ">
+                    <i className="fas fa-trash"></i>
+                </button>
+                <i className="fas fa-star"></i>
+            </div>
+        </li>
+    )
 }
 
-export default EmployeesListItem
+export default EmployeesListItem;
