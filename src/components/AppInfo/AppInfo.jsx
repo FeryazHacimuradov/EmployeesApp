4ee473afa89af264f8bb5183822c38cf71da32c6
FeryazHacimuradov <tu201906125@code.edu.az>
2022-08-19 17:07:17 +0400

    created AppInfo, connected Tailwind

--- src/components/AppInfo/AppInfo.jsx
+++ src/components/AppInfo/AppInfo.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './appInfo.css';
 
 const AppInfo = () => {
   return (
-    <div>AppInfo</div>
+    <div className='mt-24'>
+      <div className=''>
+        <div className="app-info mx-48 text-2xl rounded bg-white dark:text-gray-200 dark:bg-secondary-dark-bg"
+              style={{ backgroundColor: '#3d5a81' }}
+              >
+          <h1>Accounting for employees in company HAJIMURADOV's</h1>
+          <h2>Total number of employees:</h2>
+          <h2>The award will be given to:</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default AppInfo
+export default AppInfo;
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import AppInfo from '../AppInfo/AppInfo';
+import SearchPanel from '../SearchPanel/SearchPanel';
+import AppFilter from '../AppFilter/AppFilter';
+import EmployeesList from '../EmployeesList/EmployeesList';
+import EmployeesAddForm from '../EmployeesAddForm/EmployeesAddForm';
 
-import React from 'react';
+import './app.css';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+            <SearchPanel/>
+            <AppFilter/>
+        </div>
+        
+        <EmployeesList/>
+        <EmployeesAddForm/>
+    </div>
+  );
 }
 
 export default App;
